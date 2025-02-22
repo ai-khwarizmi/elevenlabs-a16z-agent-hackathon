3a2eb317c14ab85627282bda67bec9bc2bc49def
Till Müller <github@till.dev>
2025-02-22 09:22:06 +0100

    simple main loop

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	let executionTime = $state(0);
+
+	$effect(() => {
+		let timeoutId: number;
+
+		function tick() {
+			const start = performance.now();
+
+			executionTime = performance.now() - start;
+			timeoutId = setTimeout(tick, 1000);
+		}
+
+		tick();
+		return () => clearTimeout(timeoutId);
+	});
 </script>
+
+<div class="fixed top-2 right-2 font-mono text-sm">
+	Task took: {executionTime.toFixed(1)}ms
+</div>
 
 {@render children()}
