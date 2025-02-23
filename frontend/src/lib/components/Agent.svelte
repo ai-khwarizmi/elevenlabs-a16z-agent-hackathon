218ff5e4f41ea876a3135bb90d5cec0cbbf52c29
Henrik Schwebcke <58189782+overiit@users.noreply.github.com>
2025-02-23 15:19:00 +0100

    call connection state

--- frontend/src/lib/components/Agent.svelte
+++ frontend/src/lib/components/Agent.svelte
@@ -87,14 +87,26 @@
 			{/if}
 		</div>
 		<!-- State text -->
-		<span
-			class="text-center font-['Anonymous_Pro'] text-lg font-bold {agent.getState() ===
-				'VOICE_ACTIVE' || agent.getState() === 'TEXT_ACTIVE'
-				? 'text-[#FF6222]'
-				: 'text-white'}"
-		>
-			{agent.getState()}
-		</span>
+		{#if agent.getState() === 'VOICE_ACTIVE'}
+			{#if agent.getIsConnectedToConversation()}
+				<span class="text-center font-['Anonymous_Pro'] text-lg font-bold text-green-500">
+					Connected
+				</span>
+			{:else}
+				<span class="text-center font-['Anonymous_Pro'] text-lg font-bold text-red-500">
+					Connecting...
+				</span>
+			{/if}
+		{:else}
+			<span
+				class="text-center font-['Anonymous_Pro'] text-lg font-bold {agent.getState() ===
+					'VOICE_ACTIVE' || agent.getState() === 'TEXT_ACTIVE'
+					? 'text-[#FF6222]'
+					: 'text-white'}"
+			>
+				{agent.getState()}
+			</span>
+		{/if}
 	</div>
 </div>
 
